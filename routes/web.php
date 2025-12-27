a206f39d8bc9b418a8d743af9661091eca42d517
tiffany061103 <137292277+tiffany061103@users.noreply.github.com>
2025-12-27 13:16:18 +0700

    Add checkout view and update routes for menu, cart, and checkout functionality

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('menu');
 });
+
+Route::get('/menu', [MenuController::class, 'index'])->name('menu');
+Route::get('/cart', [MenuController::class, 'cart'])->name('cart');
+Route::post('/cart/add', [MenuController::class, 'addToCart'])->name('cart.add');
+
+Route::get('/checkout', function () {
+    return view('customer.checkout');
+})->name('checkout');
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,5 +1,6 @@
 <?php
 
+/** @var \Tests\TestCase $this */
 test('the application returns a successful response', function () {
     $response = $this->get('/');
 
